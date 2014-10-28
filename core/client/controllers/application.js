6e32628c72cd4aa5307ce562daa06c844d7dba38
Robert Jackson <robert.w.jackson@me.com>
2014-10-28 08:56:05 -0400

    Fix invalid templating in navbar partial.

--- core/client/controllers/application.js
+++ core/client/controllers/application.js
@@ -3,6 +3,12 @@ var ApplicationController = Ember.Controller.extend({
     hideNav: Ember.computed.match('currentPath', /(error|signin|signup|setup|forgotten|reset)/),
     // jscs: enable
 
+    sessionUserImageAltText: Ember.computed('session.user.image', 'session.user.name', function () {
+        var name = this.get('session.user.name');
+
+        return name + '\'s profile picture';
+    }),
+
     topNotificationCount: 0,
     showGlobalMobileNav: false,
     showSettingsMenu: false,
